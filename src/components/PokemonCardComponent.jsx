21451d15ce37f26e33a5142419cb9b54c2857ec9
Fr4n0m <fran11799@outlook.com>
2024-01-28 19:30:19 +0100

    Remove unused code for getting type color classes.

--- src/components/PokemonCardComponent.jsx
+++ src/components/PokemonCardComponent.jsx
@@ -7,10 +7,6 @@ import React, { useState, useEffect } from "react";
 import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
 import Link from "next/link";
 import "animate.css/animate.min.css";
-import {
-  getTypeColorClass,
-  getHoverTypeColorClass,
-} from "@/utils/colorTypeClass";
 
 const PokemonCard = ({ pokemonId, index }) => {
   const pokemon = getPokemonById(pokemonId);
@@ -59,6 +55,57 @@ const PokemonCard = ({ pokemonId, index }) => {
 
   const saveFavoritesToLocalStorage = (updatedFavorites) => {
     localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  };
+
+  //Función que devuelve estilos depende del tipo de pokemonexport
+  const getTypeColorClass = (type) => {
+    switch (type) {
+      case "grass":
+        return "bg-[#3fab85] border-green-900 text-green-900";
+      case "fire":
+        return "bg-[#e57e43] border-orange-800 text-orange-800";
+      case "water":
+        return "bg-[#5a8ed3] border-blue-900 text-blue-900";
+      case "bug":
+        return "bg-[#b2f06c] border-[#698256] text-[#698256]";
+      case "poison":
+        return "bg-[#c8a0ef] border-[#5d4a70] text-[#5d4a70]";
+      case "flying":
+        return "bg-[#83baf0] border-[#415c7a] text-[#415c7a]";
+      case "electric":
+        return "bg-[#f3ca04] border-[#b19416] text-[#b19416]";
+      case "ground":
+        return "bg-[#cac48e] border-[#636047] text-[#636047]";
+      case "fairy":
+        return "bg-[#f7a7b8] border-[#bb5c72] text-[#bb5c72]";
+      default:
+        return "bg-gray-400 border-gray-600 text-gray-600";
+    }
+  };
+
+  const getHoverTypeColorClass = (type) => {
+    switch (type) {
+      case "grass":
+        return "bg-[#205242] border-[#3fab86] text-[#3fab86]";
+      case "fire":
+        return "bg-[#7e4629] border-[#e57e43] text-[#e57e43]";
+      case "water":
+        return "bg-[#294161] border-[#5a8ed3] text-[#5a8ed3]";
+      case "bug":
+        return "bg-[#557536] border-[#b2f06c] text-[#b2f06c]";
+      case "poison":
+        return "bg-[#65517c] border-[#c8a0ef] text-[#c8a0ef]";
+      case "flying":
+        return "bg-[#456584] border-[#83baf0] text-[#83baf0]";
+      case "electric":
+        return "bg-[#897311] border-[#f3ca04] text-[#f3ca04]";
+      case "ground":
+        return "bg-[#636047] border-[#cac48e] text-[#cac48e]";
+      case "fairy":
+        return "bg-[#8b4554] border-[#f7a7b8] text-[#f7a7b8]";
+      default:
+        return "bg-gray-700 border-gray-400 text-gray-400";
+    }
   };
 
   //Definicion de colorClass para cambiar el color de la card depende del tipo de pokemon, hace uso de la función definida arriba
